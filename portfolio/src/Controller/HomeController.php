97b7a4cfac2a5039ebe9f7eaa45f3c83f9a8c388
vi <fish.design.co@gmail.com>
2020-10-05 09:54:08 +0200

    user+loginauthentificator

---
+++ portfolio/src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function __invoke()
+    {
+        return $this->render('home.html.twig');
+    }
+   
+}
